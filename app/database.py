0e9ae05dbdd71c2d2bd0bfbbaa40f96f2453939a
krishika <krishikakh@gmail.com>
2026-05-25 12:39:19 +0545

    .

--- app/database.py
+++ app/database.py
@@ -1,20 +1,23 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# MySQL connection string format: mysql+pymysql://user:password@host:port/database
-DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:@localhost:3306/notely_db")
 
+DATABASE_URL = os.getenv("DATABASE_URL") or os.getenv("MYSQL_URL")
+
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL environment variable not set!")
+
+# For MySQL on Railway, we don't need the special connect_args usually.
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
